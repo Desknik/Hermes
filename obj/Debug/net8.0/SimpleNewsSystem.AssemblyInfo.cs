9967ffb69e7ae78feb4081e4b9a7a585de4a0327
Arthvz <arthuverdadeiro@outlook.com>
2024-06-03 14:42:32 -0300

    feat: Visual itens change

--- obj/Debug/net8.0/SimpleNewsSystem.AssemblyInfo.cs
+++ obj/Debug/net8.0/SimpleNewsSystem.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SimpleNewsSystem")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0e62f5434f2525a71fccefd2380a321e78506c38")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c09fe7687a14f1aa2d007e328b787cba46803e71")]
 [assembly: System.Reflection.AssemblyProductAttribute("SimpleNewsSystem")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SimpleNewsSystem")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
